4c33116a2ea0394c87eed3aff875fea939541a3d
unknown <tallen904@gmail.com>
2018-03-06 22:52:26 -0800

    commit

---
+++ server.js
@@ -0,0 +1,34 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+// require("./routes/api-routes.js")(app);
+require('./routes/html-routes.js')(app);
+
+// Server Listener
+
+app.listen(PORT, function() {
+  console.log(`App listening on port: ${PORT}`);
+});
